2055ec70abb4d4d90f54555ca48fb798fe911abd
Dieter Stinglhamber <elhebert@hotmail.be>
2023-01-25 22:57:08 +0100

    update footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="w-full text-center p-8 border-t-2 border-dashed border-t-[#ECC127] text-sm">
 
     <p class="mb-5">
-        This website source code can be found on <CtaLink href="https://github.com/elhebert/dieter.dev">GitHub</CtaLink>.
+        This website source code can be found on <a class="no-underline hover:underline font-bold text-[#3736C6]" href="https://github.com/elhebert/dieter.dev">GitHub</a>.
     </p>
     <p class="my-5">
         Made by <a class="no-underline hover:underline font-bold text-[#3736C6]" href="/">Elhebert</a> using&nbsp;<a class="no-underline hover:underline font-bold text-[#3736C6]" href="https://tailwindcss.com">Tailwind CSS</a>, powered by&nbsp;<a class="no-underline hover:underline font-bold text-[#3736C6]" href="https://laravel.com/">Laravel</a> and hosted on&nbsp;<a class="no-underline hover:underline font-bold text-[#3736C6]" href="https://www.netlify.com">Netlify</a>.
